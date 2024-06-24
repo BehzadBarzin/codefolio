029df5319c47bf93e115c172e2eeee1c78253101
Behzad Barzin <behzad_barzin@live.com>
2024-06-24 05:10:07 +0330

    Implements Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/constants";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex items-center justify-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
+
+        {/* TEMP: Simple Long Section to see nav */}
+        <div className="w-full h-[500vh]"></div>
       </div>
     </main>
   );
