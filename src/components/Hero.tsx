a82c153c1954512b7867f022aa9843a14201370e
Behzad Barzin <behzad_barzin@live.com>
2024-06-24 05:05:37 +0330

    Completed Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,7 @@
 import { Spotlight } from "@/components/ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
 
 const Hero = () => {
   return (
@@ -21,11 +24,31 @@ const Hero = () => {
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
 
+      {/* Content */}
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          {/* Sub title */}
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Web and Mobile Developer
           </h2>
+          {/* title */}
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl font-bold"
+          />
+          {/*  */}
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-xl">
+            Hi, I&apos;m Behzad, a Fullstack Developer
+          </p>
+
+          {/* Button */}
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
     </div>
