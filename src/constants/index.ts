5469442c6a56ad481c203ffebe84fad7a548a697
Behzad Barzin <behzad_barzin@live.com>
2024-06-25 19:27:16 +0330

    Creates Experiences section

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -165,29 +165,29 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
-    className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
-  },
-  {
-    id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Mobile App Dev",
+    desc: "Designed and developed mobile app for Android platform using Java.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
+    id: 2,
+    title: "Junior Fullstack Engineer",
+    desc: "Development of a web-based platforms using React.js, PHP, and Node.js, enhancing the user experience.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
     id: 3,
     title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    desc: "Led the dev of mobile and web apps for clients, from initial concept to deployment.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Senior Fullstack Developer",
+    desc: "Developed and maintained both server and client applications using modern frontend and backend technologies.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
